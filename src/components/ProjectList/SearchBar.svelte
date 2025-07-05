9de037a893e18308994743f8b58a66f00b53e61c
StarlitEvelyn <evelyn.rei@protonmail.com>
2025-07-05 12:03:15 +0200

    Added clear search button

--- src/components/ProjectList/SearchBar.svelte
+++ src/components/ProjectList/SearchBar.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import { DeleteIcon } from "lucide-svelte";
+
 	let { query = $bindable() }: { query: string } = $props();
 </script>
 
-<div class="flex p-2 rounded m-2 gap-2 bg-zinc-900 text-white w-full">
+<div class="relative flex p-2 rounded m-2 gap-2 bg-zinc-900 text-white w-full">
 	<input
 		class="w-full outline-none"
 		type="text"
@@ -11,4 +13,12 @@
 	/>
 	<!-- Max length of a filename in most filesystems -->
 	<!--! If want search to work with paths, must increase limit!  -->
+	<button
+		onclick={() => {
+			query = "";
+		}}
+		class="absolute right-2 top-1 cursor-pointer hover:bg-zinc-700 p-1 rounded"
+	>
+		<DeleteIcon />
+	</button>
 </div>

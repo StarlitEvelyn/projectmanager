2e7e0b002748e4fbbdb8df58362aba806ad9e762
StarlitEvelyn <evelyn.rei@protonmail.com>
2025-07-06 20:39:32 +0200

    Added functionality for .code-workspace files to be opened

--- src/components/ProjectDetails/ProjectActions.svelte
+++ src/components/ProjectDetails/ProjectActions.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { invoke } from "@tauri-apps/api/core";
+	import { join } from "@tauri-apps/api/path";
+	import { readDir } from "@tauri-apps/plugin-fs";
 	import { openUrl } from "@tauri-apps/plugin-opener";
 	import type { Project } from "@type/project";
 
@@ -9,8 +11,23 @@
 	const { project }: Props = $props();
 
 	const openInEditor = async () => {
-		await invoke("open_vscode", { path: project.path });
-		console.log(`Opening ${project.path}`);
+		let openPath = project.path;
+
+		// Check if there's a .code-workspace file
+		const projectFiles = await readDir(project.path);
+		const workspaceFile = projectFiles.find((file) => {
+			const fileExtension =
+				file.name.split(".")[file.name.split(".").length - 1];
+
+			return fileExtension === "code-workspace";
+		});
+
+		// If there is a .code-workspace file update path to open it
+		if (workspaceFile) openPath = await join(project.path, workspaceFile.name);
+
+		// If not open folder in VSC
+		await invoke("open_vscode", { path: openPath });
+		console.log(`Opening ${openPath}`);
 	};
 
 	const openGitHub = async () => {
@@ -20,6 +37,12 @@
 </script>
 
 <div class="flex gap-2 justify-evenly mt-auto flex-wrap">
-	<button class="p-2 rounded cursor-pointer bg-zinc-800 flex-1/3 text-nowrap" onclick={openInEditor}>Open in Editor</button>
-	<button class="p-2 rounded cursor-pointer bg-zinc-800 flex-1/3 text-nowrap" onclick={openGitHub}>Open on github</button>
+	<button
+		class="p-2 rounded cursor-pointer bg-zinc-800 flex-1/3 text-nowrap"
+		onclick={openInEditor}>Open in Editor</button
+	>
+	<button
+		class="p-2 rounded cursor-pointer bg-zinc-800 flex-1/3 text-nowrap"
+		onclick={openGitHub}>Open on github</button
+	>
 </div>
